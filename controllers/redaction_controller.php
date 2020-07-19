cd4b3d89c8140cd036041c9b544ae51f1f4ba532
Helfina <53316318+helfina@users.noreply.github.com>
2020-07-19 17:09:36 +0200

    espace admin redaction correction faute de frappe

--- controllers/redaction_controller.php
+++ controllers/redaction_controller.php
@@ -1,6 +1,6 @@
 <?php
 if(isset($_POST['article_titre'], $_POST['article_contenu'])){
-    if(!empty($_POST[article_titre]) AND !empty($_POST['article_contenu'])){
+    if(!empty($_POST['article_titre']) AND !empty($_POST['article_contenu'])){
 
     }else{
         $error = 'Veuillez remplir tous les champs !';
