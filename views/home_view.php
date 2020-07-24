6a7e63123f8463f1be6364dad49acb72acc871f5
Helfina <53316318+helfina@users.noreply.github.com>
2020-07-24 22:43:33 +0200

    corection html 3wvalidator

--- views/home_view.php
+++ views/home_view.php
@@ -155,7 +155,7 @@
             <article class="carousel">
 
                 <div class="precedent" onclick=" ChangeSlide(-1) ">
-                    <svg whidth="50px" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
+                    <svg  xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24"
                         fill="black">
                         <path d="M15.41 7.41L14 6l-6 6 6 6 1.41-1.41L10.83 12z" />
                         <path d="M0 0h24v24H0z" fill="none" />
@@ -236,7 +236,7 @@
                     </figure>
                 </div>
                 <div class="suivant" onclick=" ChangeSlide (1) ">
-                    <svg whidth="50px" height="50px" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" >
+                    <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="black" >
                         <path d="M10 6L8.59 7.41 13.17 12l-4.58 4.59L10 18l6-6z" />
                         <path d="M0 0h24v24H0z" fill="none" />
                     </svg>
