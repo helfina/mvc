a81406545cd83eb8f03c98fbc7ece0c339cf741e
Helfina <53316318+helfina@users.noreply.github.com>
2020-07-25 15:57:38 +0200

    foter mediaqueries

--- views/includes/footer.php
+++ views/includes/footer.php
@@ -1,15 +1,18 @@
-<footer class="footer">
-        <section class="bloc_footer">
-            <article class="ad_boutique">
-                <h2>Nos Boutiques</h2>
-                <address>
-                    <div>
+<footer class="container footer">
+        <div class=" row bloc_footer">
+
+            <article class="col-s-12 col-txs-12  col-4 address_boutique">
+                <h3>Nos Boutiques</h3>
+                <address class="row">
+                    <div class="col-s-12 col-m-6 col-txs-6 col-6">
+                        <h5>St-Jean-Brevelay</h5>
                         1 route de vannes<br>
                         56390 Grand-Champ<br>
                         <a href="tel:+33952461614">Tel : 09.52.46.16.14</a>
                     </div>
 
-                    <div>
+                    <div class="col-s-12 col-m-6 col-txs-6 col-6">
+                    <h5>Grand-Champs</h5>
                         2 Rue de buléon<br>
                         56660 Saint-Jean-Brevelay<br>
                         <a href="tel:+33297604165"> Tel : 02.97.60.41.65</a>
@@ -17,27 +20,29 @@
                 </address>
             </article>
 
-            <article class="partenaire">
-                <h2>Partenaire</h2>
-                <aside class="logo_partenaire">
-                    <a
-                        href="https://www.florajet.com/?opm=3909&gclid=CjwKCAjwg6b0BRBMEiwANd1_SPg7DdaLB_SJu-VaEzkYDLAB62_t6s8l_-IoQwVMDiMID1dVFv1SMRoCD_sQAvD_BwE"><img
-                            src="assets/images/logo_partenaire.png" alt="Florajet"></a>
-                    <a href="https://www.123fleurs.com/"><img src="assets/images/logo-partenaire123.jpg"
-                            alt="123 Fleurs"></a>
+            <article class="col-s-12 col-txs-5 col-4 partenaire">
+                <h3>Partenaire</h3>
+                <aside class="row logo_partenaire">
+                    <a class="col-s-12 col-m-6 col-txs-12 col-6" href="https://www.florajet.com/?opm=3909&gclid=CjwKCAjwg6b0BRBMEiwANd1_SPg7DdaLB_SJu-VaEzkYDLAB62_t6s8l_-IoQwVMDiMID1dVFv1SMRoCD_sQAvD_BwE">
+                        <img src="assets/images/logo_partenaire.png" alt="Florajet">
+                    </a>
+                    <a class="col-s-12 col-m-6 col-txs-12 col-6" href="https://www.123fleurs.com/" >
+                        <img src="assets/images/logo-partenaire123.jpg" alt="123 Fleurs">
+                    </a>
 
                 </aside>
             </article>
-            <article class="liens">
-                <h2>Liens</h2>
-                <ul>
-                    <li><a href="mentionLegale.html">Mention Légal</a></li>
-                    <li><a href="#">Politique Cookies</a></li>
-                    <li><a href="#">Condition Général de Vente</a></li>
-                    <li><a href="planDuSite.html">plan du site</a></li>
+
+            <article class="col-s-12 col-txs-5 col-4 liens">
+                <h3>Liens</h3>
+                <ul class="row">
+                    <li class="col-s-12 col-m-6 col-txs-12 col-6"><a href="mentionLegale.html">Mention Légal</a></li>
+                    <li class="col-s-12 col-m-6 col-txs-12 col-6"><a href="#">Politique Cookies</a></li>
+                    <li class="col-s-12 col-m-6 col-txs-12 col-6"><a href="#">Condition Général de Vente</a></li>
+                    <li class="col-s-12 col-m-6 col-txs-12 col-6"><a href="planDuSite.html">plan du site</a></li>
                 </ul>
             </article>
-        </section>
+        </div>
         <p class="end">© Gaëlle Kerforne 2020 tous droits réservés</p>
     </footer>
 
