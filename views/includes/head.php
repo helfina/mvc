970a203f513fc3049cdd38231d5c2ae014a72181
Helfina <53316318+helfina@users.noreply.github.com>
2020-07-26 14:20:02 +0200

    corectif 3wvalidator

--- views/includes/head.php
+++ views/includes/head.php
@@ -1,9 +1,9 @@
 <!-- Meta Tags -->
 <!-- <meta charset="UTF-8" /> -->
-<meta http-equiv="Content-Type" content="text/html"; charset="UTF-8">
+<meta http-equiv="Content-Type" content="text/html";charset="UTF-8">
 <meta name="viewport" content="width=device-width, user-scalable=0, maximum-scale=1, initial-scale=1.0, maximum-scale=1">
 <meta name="author" content="<?= WEBSITE_AUTHOR?>">
-<meta name="description" content="<?= WEBSITE_DESCRIPTION?>" />
+<meta name="description" content="<?= WEBSITE_DESCRIPTION?>"/>
 <meta name=”keywords” content="<?= WEBSITE_KEYWORDS?>" />
 <meta name="Reply-to" content="<?= WEBSITE_AUTHOR_MAIL?>">
 <meta name="Copyright" content="<?= WEBSITE_AUTHOR?>">
